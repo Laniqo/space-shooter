d0302d252128dc37eda7bbf726b033929e79d0d9
undefxned <undefxned.variable>
2017-12-11 04:18:55 +0200

    Scripts
    uploaded scripts from tutorial

---
+++ DestroyByBoundary.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByBoundary : MonoBehaviour {
+
+
+	//this is called when the other collider(shot) has stopped touching the boundary collider
+	// the shot is then destroyed
+	void OnTriggerExit(Collider other){
+		Destroy (other.gameObject);
+	}
+		
+
+}
